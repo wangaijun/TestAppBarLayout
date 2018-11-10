0c86601c8ef4d34c5f7f91ddc5e82f73aa726cde
waj <wangaj@glodon.com>
2018-11-10 20:19:03 +0800

    删除无关的代码

--- app/src/main/java/com/waj/testappbarlayout/MainActivity.kt
+++ app/src/main/java/com/waj/testappbarlayout/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.waj.testappbarlayout
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
