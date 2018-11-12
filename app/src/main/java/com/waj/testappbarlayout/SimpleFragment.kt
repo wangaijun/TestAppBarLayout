3af9ae1d13997b306e4a9a295d0044def89a1b05
waj <wangaj@glodon.com>
2018-11-12 16:41:00 +0800

    添加注释

--- app/src/main/java/com/waj/testappbarlayout/SimpleFragment.kt
+++ app/src/main/java/com/waj/testappbarlayout/SimpleFragment.kt
@@ -8,10 +8,12 @@ import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_main.*
 
 class SimpleFragment : Fragment(){
+    /**加载布局文件*/
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_main,container,false)
     }
 
+    /**显示数据*/
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val selectionNum = arguments?.getString("selectionNum")
@@ -25,6 +27,7 @@ class SimpleFragment : Fragment(){
     }
 
     companion object {
+        /**静态接口封装类的创建，规范Fragment的创建*/
         fun newInstance(selectionNum: String):Fragment {
             val fragment = SimpleFragment()
             val args = Bundle()
