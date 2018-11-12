a609d16a93f6adbd39fd5b6d80165862a962bd58
waj <wangaj@glodon.com>
2018-11-12 19:36:13 +0800

    添加注释

--- app/src/main/java/com/waj/testappbarlayout/SimpleAdapter.kt
+++ app/src/main/java/com/waj/testappbarlayout/SimpleAdapter.kt
@@ -14,12 +14,15 @@ class SimpleAdapter(fm: FragmentManager?) : FragmentPagerAdapter(fm) {
         return 3
     }
 
+    //为联动的tablayout提供标签名
+    override fun getPageTitle(position: Int): CharSequence? = "第${position}页"
+
+    //非必须，为顶部的动画提供响应的图片
+    //@DrawableRes,强制编译器在编译阶段检查所传递的int值是否为图片资源，是否不靠谱
     @DrawableRes fun getDrawable(endPosition: Int) =
         when(endPosition){
             0->R.drawable.m1
             1->R.drawable.m2
             else->R.drawable.m3
         }
-
-    override fun getPageTitle(position: Int): CharSequence? = "第${position}页"
 }
